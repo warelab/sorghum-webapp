80313789a529c264841bfed8a966c70f5e3f30d2
Andrew Olson <olson@cshl.edu>
2022-02-05 01:13:52 -0500

    pubmed updater

--- sorghum_webapp/sorghum_webapp/utilities/pubmedIDpull.py
+++ sorghum_webapp/sorghum_webapp/utilities/pubmedIDpull.py
@@ -3,7 +3,7 @@ from litter_getter import pubmed
 import xml.etree.ElementTree as ET
 
 # register with user account
-pubmed.connect('muna@cshl.edu')
+pubmed.connect('68732cb76781df2d9ad6d6197712a42b7108')
 
 def getMetaData(papersToFind):
 
@@ -16,10 +16,16 @@ def getMetaData(papersToFind):
 
 	for num, id in enumerate(refs):
 		papersToFind[num].s.abstract = refs[num]['abstract']
-		papersToFind[num].s.paper_authors = ', '.join(refs[num]['authors_list'])
+		papersToFind[num].s.paper_authors = ', '.join(refs[num]['authors'])
+		papersToFind[num].s.title = refs[num]['title']
 		papersToFind[num].s.source_url = "https://www.ncbi.nlm.nih.gov/pubmed/" + papersToFind[num].s.pubmed_id
+		papersToFind[num].s.doi = refs[num]['doi']
+		papersToFind[num].s.abstract = refs[num]['abstract']
 
 		root = ET.fromstring(refs[num]["xml"])
+		if root[0].find('Article'):
+			if root[0].find('Article').find('Journal'):
+				papersToFind[num].s.journal = root[0].find('Article').find('Journal').find('Title').text
 
 		for pubDate in root[1][0].findall('PubMedPubDate'):
 			if pubDate.get('PubStatus') == 'pubmed':
