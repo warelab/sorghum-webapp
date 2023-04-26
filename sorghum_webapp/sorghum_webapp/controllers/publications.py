001900dd440c00f571e1f885ea604ee43d1fb839
Andrew Olson <olson@cshl.edu>
2023-04-26 12:17:08 -0400

    paginated publications page

--- sorghum_webapp/sorghum_webapp/controllers/publications.py
+++ sorghum_webapp/sorghum_webapp/controllers/publications.py
@@ -26,22 +26,26 @@ app_logger = logging.getLogger("sorghumbase")
 
 publications_page = flask.Blueprint("publications_page", __name__)
 
-def getPapers(current_page, per_page, paper_tally, tag_filter, update_all):
+def getPapers(current_page, per_page, paper_tally, tag_filter, before, after, show_all, force_update):
     updatedPapers = []
-    while update_all and per_page * (current_page-1) < paper_tally :
-        updatedPapers.push(getPapers(current_page, per_page, paper_tally, tag_filter, False))
+    while show_all and per_page * (current_page-1) < paper_tally :
+        updatedPapers += getPapers(current_page, per_page, paper_tally, tag_filter, before, after, False, force_update)
         current_page = current_page + 1
-    if not update_all:
+    if not show_all:
         paper_request = ScientificPaperRequest(api=api)
         if tag_filter:
-            paper_request.tags = [tag_filter]
+            paper_request.tags = tag_filter
+        if before:
+            paper_count.before= before
+        if after:
+            paper_count.after= after
         paper_request.per_page = per_page
         paper_request.page = current_page
         page_of_papers = paper_request.get()
 
         queryPubmed = []
         for p in page_of_papers :
-            if len(p.s.pubmed_id) > 0 and len(p.s.content) == 0 :
+            if len(p.s.pubmed_id) > 0 and (force_update or len(p.s.content) == 0) :
                queryPubmed.append(p)
             else :
                updatedPapers.append(p)
@@ -66,6 +70,7 @@ def getPapers(current_page, per_page, paper_tally, tag_filter, update_all):
                     paper.s.content += "\n" + paper.s.pubmed_id
                     if paper.s.doi:
                         paper.s.content += "\n" + paper.s.doi
+                    print("calling paper.update()", paper.s.date)
                     paper.update()
                     print("updated paper", paper.s.pubmed_id, paper.s.title)
                     updatedPapers.append(paper)
@@ -79,77 +84,70 @@ WAY_MORE_THAN_WE_WILL_EVER_HAVE = 100
 def publications():
     ''' List of research papers '''
     templateDict = navbar_template('Research')
-    update_all = valueFromRequest(key="update_by_pubmed", request=request, boolean=True) or False
-    tag_filter = valueFromRequest(key="tag", request=request)
+    show_all = valueFromRequest(key="show_all", request=request, boolean=True) or False
+    force_update = valueFromRequest(key="force_update", request=request, boolean=True) or False
+    tag_filter = valueFromRequest(key="tag", request=request, aslist=True)
+    before = valueFromRequest(key="before", request=request)
+    after = valueFromRequest(key="before", request=request)
     current_page = valueFromRequest(key="page", request=request, integer=True) or 1
     per_page = valueFromRequest(key="per_page", request=request, integer=True) or 100
     with api.Session():
         paper_count = ScientificPaperRequest(api=api)
         if tag_filter:
-            paper_count.tags = [tag_filter]
+            paper_count.tags = tag_filter
+        if before:
+            paper_count.before = before
+        if after:
+            paper_count.after = after
         paper_count.per_page = 1
         paper_count.page = 1
         paper_tally = paper_count.get(count=True)
 
-        updatedPapers = getPapers(current_page, per_page, paper_tally, tag_filter, update_all)
+        updatedPapers = getPapers(1, 100, paper_tally, tag_filter, before, after, True, force_update)
 
-        all_years = []
         tag_freq = {}
-        pubmed_count = {}
 
-        sortedPapersByDate = sorted(updatedPapers, reverse=True, key=lambda k: k.s.publication_date)
-        papersByDate = []
-        for paper in sortedPapersByDate:
-            if paper.s.pubmed_id:
-                if paper.s.pubmed_id not in pubmed_count:
-                    pubmed_count[paper.s.pubmed_id] = 1
-                    papersByDate.append(paper)
-                else:
-                    pubmed_count[paper.s.pubmed_id] += 1
-                    print("duplicated pubmed_id ",paper.s.pubmed_id)
-            else:
-                papersByDate.append(paper)
-
+        for paper in updatedPapers:
             for tag in paper.s.tags:
                 if tag not in tag_freq:
                     tag_freq[tag] = 1
                 else:
                     tag_freq[tag] += 1
-            if paper.s.publication_date[:4] not in all_years:
-                all_years.append(paper.s.publication_date[:4])
-
-        print("total number of papers: ", len(sortedPapersByDate))
-        print("unique papers: ", len(papersByDate))
-        templateDict['papers'] = papersByDate
-        templateDict['years'] = all_years
+        first=0
+        last=paper_tally
+        if not show_all:
+            first = per_page* (current_page-1)
+            last = per_page * current_page
+            if last > paper_tally:
+                last = paper_tally
+        templateDict['papers'] = updatedPapers[first:last]
         templateDict['page'] = current_page
         templateDict['n_pages'] = math.ceil(paper_tally / per_page)
+        templateDict['n_papers'] = paper_tally
+        if math.ceil(paper_tally / per_page) > 1:
+            templateDict['url'] = f"/publications?per_page={per_page}"
+            if tag_filter:
+                templateDict['url'] += f"&tag={tag_filter}"
+            templateDict['url'] += "&page="
         tags_tally = 0
         min_2_tags = {key: value for (key, value) in sorted(tag_freq.items(), reverse=True, key=lambda t: t[1]) if value > 0 }
         tlist= list(min_2_tags.keys())
         tag_names = {}
-        step=200
-        for i in range(0,len(tlist), step):
-            tag_counter = TagRequest(api=api)
-            tag_counter.per_page = 1
-            tag_counter.page = 1
-            tag_counter.include = ','.join(map(str,tlist[i:i+step]))
-            tag_counter.populate_request_parameters()
-            tags_tally = tags_tally + tag_counter.get(count=True)
-            tag_page = 0
-            tags_per_page = 100
-            while tags_per_page * tag_page < tags_tally :
-                tag_page = tag_page + 1
-                tag_getter = TagRequest(api=api)
-                tag_getter.per_page = tags_per_page
-                tag_getter.page = tag_page
-                tag_getter.include = tag_counter.include
-                tag_getter.populate_request_parameters()
-                page_of_tags = tag_getter.get()
-                for t in page_of_tags :
-                    tag_names[t.s.id] = t.s.name
+        tags_per_page=100
+        tag_page = 0
+        for i in range(0,len(tlist), tags_per_page):
+            tag_page += 1
+            tag_getter = TagRequest(api=api)
+            tag_getter.per_page = tags_per_page
+            tag_getter.page = tag_page
+            tag_getter.include = ','.join(map(str,tlist[i:i+tags_per_page]))
+            tag_getter.populate_request_parameters()
+            page_of_tags = tag_getter.get()
+            for t in page_of_tags :
+                tag_names[t.s.id] = t.s.name
         templateDict['tags'] = min_2_tags
         templateDict['tagname'] = tag_names
+        templateDict['tagfreq'] = tag_freq
 
     news_banner_media = api.media(slug="sorghum_panicle")
     templateDict["banner_media"] = news_banner_media
